ecb2310d479b499f657c038d1dae673f57746434
Zachary G <zachary.gethin@destronia.com>
2024-03-31 15:55:21 +0800

    fix version

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-## Version: 0.1.
+## Version: 0.1.1
 ## Git Repository: https://github.com/redtrillix/DiscordComputerStatus
 ## License: https://github.com/redtrillix/DiscordComputerStatus/raw/main/LICENSE
 ## Owner: Zachary G (redtrillix)
